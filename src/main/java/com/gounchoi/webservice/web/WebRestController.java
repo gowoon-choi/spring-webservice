d3fbb4f7d6c828109b00472eda865950d5bd5f05
Goun Choi <gounchoi@Gounui-MacBookPro.local>
2020-09-22 02:01:34 +0900

    feat1 : add controller and dto

--- src/main/java/com/gounchoi/webservice/web/WebRestController.java
+++ src/main/java/com/gounchoi/webservice/web/WebRestController.java
@@ -1,13 +1,24 @@
 package com.gounchoi.webservice.web;
 
+import com.gounchoi.webservice.domain.posts.PostsRepository;
+import com.gounchoi.webservice.dto.PostsSaveRequestDto;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class WebRestController {
+
+    private PostsRepository postsRepository;
 
     @GetMapping("/hello")
     public String hello(){
         return "HelloWorld";
+    }
+
+    public void savePosts(@RequestBody PostsSaveRequestDto dto){
+        postsRepository.save(dto.toEntity());
     }
 }
