317fe2d8322da30cd04325a8c21800a546ea9eee
gowoon-choi <gowoonvv@gmail.com>
2020-11-11 00:55:26 +0900

    feat: #3-1 handlebars 연동

---
+++ src/main/java/com/gounchoi/webservice/web/WebController.java
@@ -0,0 +1,15 @@
+package com.gounchoi.webservice.web;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@AllArgsConstructor
+public class WebController {
+
+    @GetMapping("/")
+    public String main(){
+        return "main";
+    }
+}
